d56fb322448d9601414d4b184f370d92d594dc89
Andrew <foki.andrew@gmail.com>
2022-06-02 22:03:28 +0300

    simple output with no save status of word

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,33 @@ import java.util.Scanner;
 
 public class Main {
 
-    String word = "Rabbit";
+    static String word = "rabbit";
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Your number is " + input.nextLine());
+        System.out.println("Guess the word");
+        int lengthWord = word.length();
+        String maskWord = "-".repeat(lengthWord);
+        System.out.println(maskWord);
+
+        do {
+            System.out.println("Please, print letter");
+            char c = input.next().charAt(0);
+            if (word.indexOf(c) >= 0 ) {
+                System.out.println("You're lucky boooy!");
+                maskWord = "";
+                for (char elem: word.toCharArray()) {
+                    if (elem == c) {
+                        maskWord+=c;
+                    } else {
+                        maskWord+="-";
+                    }
+                }
+                System.out.println(maskWord);
+            } else {
+                System.out.println("Miss(. \n Try again");
+            }
+        } while(maskWord.contains("-"));
     }
 }
